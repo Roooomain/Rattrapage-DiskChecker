ffa7d91face835cc301e1cb21206d1ea69b211a0
Roooomain <rommor2810@gmail.com>
2024-02-15 15:19:23 +0100

    feat: add console version

---
+++ DiskSpace/DiskSpaceConsole/LogManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using DiskSpaceConsole;
+
+namespace DiskSpaceConsole
+{
+    public class LogManager
+    {
+        private static readonly LogManager _instance = new LogManager();
+        private static string logFilePath = Path.Combine(Path.GetTempPath(), "logDiskSpace.json");
+
+        private LogManager() { }
+
+        public static LogManager Instance
+        {
+            get { return _instance; }
+        }
+
+        public void Log(string message)
+        {
+            File.AppendAllText(logFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+    }
+}
--- DiskSpace/DiskSpaceConsole/Program.cs
+++ DiskSpace/DiskSpaceConsole/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DiskSpaceConsole;
+
+namespace DiskSpaceConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Initialisation de l'intervalle à 10 secondes par défaut
+            int interval = 10000;
+
+            // Récupération de l'intervalle depuis les arguments
+            if (args.Length > 0 && int.TryParse(args[0], out int n))
+            {
+                // Conversion de l'intervalle en millisecondes
+                interval = n * 1000; 
+            }
+
+            // Création d'une instance de DiskSpaceManager
+            DiskSpaceManager manager = new DiskSpaceManager(interval);
+
+            // Démarrage du manager
+            manager.Start();
+
+            // Message de démarrage du programme et affichage de l'intervalle
+            Console.WriteLine(@"Le fichier log se situe au chemin : " + Path.Combine(Path.GetTempPath(), "logDiskSpace.json"));
+            Console.WriteLine(@"L'intervalle de vérification est de " + interval / 1000 + " secondes.");
+            Console.WriteLine(@"Appuyez sur une touche pour quitter.");
+            
+            // Attente d'une entrée utilisateur pour arrêter le programme
+            Console.ReadKey();
+            manager.Stop();
+        }
+    }
+}
