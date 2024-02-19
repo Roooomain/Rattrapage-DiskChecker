c34f5957be7967cfc0463bdb6340d8c5a3d1eaff
Roooomain <rommor2810@gmail.com>
2024-02-19 14:47:09 +0100

    feat: change diskspace console

---
+++ DiskSpace/DiskSpaceConsole/DiskSpaceLogger.cs
@@ -0,0 +1,19 @@
+﻿using DiskSpaceConsole.Helper;
+
+public class DiskSpaceLogger : IObserver
+{
+    private static readonly DiskSpaceLogger _instance = new DiskSpaceLogger();
+    private static string logFilePath = Path.Combine(Path.GetTempPath(), "logDiskSpace.log");
+
+    private DiskSpaceLogger() { }
+
+    public static DiskSpaceLogger Instance
+    {
+        get { return _instance; }
+    }
+
+    public void Update(DiskSpaceInfo info)
+    {
+        File.AppendAllText(logFilePath, $"{DateTime.Now}: {info.FreeSpace}{Environment.NewLine}");
+    }
+}
---
+++ DiskSpace/DiskSpaceConsole/DiskSpaceMonitor.cs
@@ -0,0 +1,53 @@
+﻿using System.Timers;
+using DiskSpaceConsole.Helper;
+using Timer = System.Threading.Timer;
+
+namespace DiskSpaceConsole;
+
+public class DiskSpaceMonitor : ISubject
+{
+    private List<IObserver> observers = new List<IObserver>();
+    private DiskSpaceInfo diskSpaceInfo;
+    
+    private System.Timers.Timer _timer;
+    private DriveInfo _driveInfo;
+
+    public DiskSpaceMonitor(int intervalInSeconds)
+    {
+        _timer = new System.Timers.Timer(intervalInSeconds * 1000);
+        _driveInfo = new DriveInfo("C");
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+    }
+
+    private void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        CheckDiskSpace();
+    }
+    
+    public void Attach(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update(diskSpaceInfo);
+        }
+    }
+
+    public void CheckDiskSpace()
+    {
+        diskSpaceInfo = new DiskSpaceInfo { FreeSpace = _driveInfo.AvailableFreeSpace, TotalSpace = _driveInfo.TotalSize };
+        Console.WriteLine($"Espace disponible sur le disque C: {diskSpaceInfo.FreeSpace} bytes");
+        Notify();
+    }   
+}
